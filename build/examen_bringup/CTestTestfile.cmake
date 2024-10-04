a2148108a770ce1ede908f91942fc99dd22e8210
Evandher Zarco <80181905+EvandherZarco25@users.noreply.github.com>
2024-10-04 14:22:39 -0600

    Delete build directory

--- build/examen_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robousr/ROS2Dev/robot_2025_ws/src/examen_bringup
-# Build directory: /home/robousr/ROS2Dev/robot_2025_ws/src/build/examen_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
